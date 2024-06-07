2ed5e15e9cc64a5047bde5192582d228ce587c1a
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-06-07 22:09:05 +0800

    feat: added intent

--- app/src/main/java/com/bangkit/trails/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/bangkit/trails/ui/welcome/WelcomeActivity.kt
@@ -3,8 +3,9 @@ package com.bangkit.trails.ui.welcome
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.trails.MainActivity
+import com.bangkit.trails.ui.main.MainActivity
 import com.bangkit.trails.databinding.ActivityWelcomeBinding
+import com.bangkit.trails.ui.onboarding.OnboardingActivity
 
 class WelcomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWelcomeBinding
@@ -16,7 +17,9 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.arrowBack.setOnClickListener {
-            onBackPressed()
+            val intent = Intent(this, OnboardingActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
         }
 
         binding.fullButton.setOnClickListener {
