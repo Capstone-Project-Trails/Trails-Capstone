2ebb7992704b0b0e8636e5e7b993bc4590df7e70
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-06-16 23:38:10 +0800

    feat: complete the searchFilter page

--- app/src/main/java/com/bangkit/trails/ui/searchfilter/SearchFilterActivity.kt
+++ app/src/main/java/com/bangkit/trails/ui/searchfilter/SearchFilterActivity.kt
@@ -1,18 +1,38 @@
-package com.bangkit.trails.ui.searchfilter
+package com.bangkitcapstone.trails.ui.searchfilter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.bangkit.trails.databinding.ActivitySearchBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkitcapstone.trails.databinding.ActivitySearchFilterBinding
 
 class SearchFilterActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySearchBinding
+    private lateinit var binding: ActivitySearchFilterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivitySearchBinding.inflate(layoutInflater)
+        binding = ActivitySearchFilterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.close.setOnClickListener {
+            onBackPressed()
+        }
 
+        binding.applyButton.setOnClickListener {
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_SELECTED_RATING, binding.optRating.text.toString())
+            resultIntent.putExtra(EXTRA_SELECTED_TYPE, binding.optType.text.toString())
+            resultIntent.putExtra(EXTRA_SELECTED_REGION, binding.optRegion.text.toString())
+            setResult(RESULT_CODE, resultIntent)
+            finish()
+        }
+
+    }
+
+    companion object {
+        const val EXTRA_SELECTED_RATING = "extra_selected_rating"
+        const val EXTRA_SELECTED_REGION = "extra_selected_region"
+        const val EXTRA_SELECTED_TYPE = "extra_selected_type"
+        const val RESULT_CODE = 110
     }
 }
