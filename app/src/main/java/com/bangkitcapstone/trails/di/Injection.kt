58581d77350da79ed322c4af140adaac9c77718d
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-06-20 21:40:47 +0800

    feat: added new base url

--- app/src/main/java/com/bangkitcapstone/trails/di/Injection.kt
+++ app/src/main/java/com/bangkitcapstone/trails/di/Injection.kt
@@ -1,11 +1,13 @@
 package com.bangkitcapstone.trails.di
 
+import com.bangkitcapstone.trails.BuildConfig
 import com.bangkitcapstone.trails.data.remote.retrofit.ApiConfig
 import com.bangkitcapstone.trails.data.repository.TrailsRepository
 
 object Injection {
     fun provideRepository(): TrailsRepository {
-        val apiService = ApiConfig.getApiService()
-        return TrailsRepository.getInstance(apiService)
+        val apiRecommendationService = ApiConfig.getApiService(BuildConfig.BASE_URL_REC)
+        val apiChatService = ApiConfig.getApiService(BuildConfig.BASE_URL_CHAT)
+        return TrailsRepository.getInstance(apiRecommendationService, apiChatService)
     }
 }
