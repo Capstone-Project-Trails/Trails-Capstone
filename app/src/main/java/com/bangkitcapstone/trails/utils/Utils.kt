4aa3747d35e3ea8a4d400b42cc18b2379cdcf667
Marcellino Candyawan <candyawanm21d@student.unhas.ac.id>
2024-06-20 21:45:02 +0800

    feat: added new convert

--- app/src/main/java/com/bangkitcapstone/trails/utils/Utils.kt
+++ app/src/main/java/com/bangkitcapstone/trails/utils/Utils.kt
@@ -1,14 +1,19 @@
 package com.bangkitcapstone.trails.utils
 
+import com.bangkitcapstone.trails.data.remote.response.Coordinates
+import com.bangkitcapstone.trails.data.remote.response.DetailData
 import com.bangkitcapstone.trails.data.remote.response.NearbyItem
+import com.bangkitcapstone.trails.data.remote.response.PopularDestinationsItem
 import com.bangkitcapstone.trails.data.remote.response.ResultsItem
 
 fun NearbyItem.toDetailData(): DetailData {
     return DetailData(
+        id = this.placeId,
         title = this.name,
         rating = this.rating,
         userRatingTotal = this.userRatingTotal,
         vicinity = null,
+        image = this.photoUrl,
         description = this.description,
         link = this.locationUrl,
         formattedAddress = this.vicinity,
@@ -19,14 +24,32 @@ fun NearbyItem.toDetailData(): DetailData {
 
 fun ResultsItem.toDetailData(): DetailData {
     return DetailData(
+        id = this.placeId,
         title = this.title,
         rating = this.rating,
         userRatingTotal = this.userRatingTotal,
         vicinity = null,
         description = this.description,
+        image = this.photoUrl,
         link = this.link,
         formattedAddress = this.formattedAddress,
         region = this.region,
         coordinates = Coordinates(this.coordinates.latitude, this.coordinates.longitude)
+    )
+}
+
+fun PopularDestinationsItem.toDetailData(): DetailData {
+    return DetailData(
+        id = this.placeId,
+        title = this.name,
+        rating = this.rating,
+        userRatingTotal = this.userRatingTotal,
+        image = this.photos,
+        vicinity = this.vicinity,
+        description = this.description,
+        link = this.locationUrl,
+        formattedAddress = null,
+        region = this.region,
+        coordinates = Coordinates(lat, lon)
     )
 }
